0e8fc7c890852c3b52e986ea9011ef422ebcc669
PuckovMark <mark.puchkov19@gmail.com>
2020-09-13 19:10:32 +0300

    add search

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -17,28 +17,28 @@ export class HomePageComponent implements OnInit {
   public data: any
   public categories: any
   public indexFavoriteItem = 0
+  public searchInput: any
 
   constructor(public dataService: DataService) {
   }
 
   ngOnInit(): void {
+    localStorage.clear()
+    this.page = 1
+    this.itemsPerPage = 7
     this.dataService.fetchData().subscribe(() => {
-      localStorage.clear()
-      this.page = 1
-      this.itemsPerPage = 7
       this.data = this.dataService.data
       this.categories = this.data.categories
-      // Sort alphabetically (numbers first)
       this.games = this.sortAlphabetically()
       this.collectionSize = this.games.length
       this.paginationArrayGeneral = this.pagination(this.games, this.collectionSize)
-      console.log(this.data)
       this.dataService.maxArraySize = this.games.length
       this.dataService.arraySize = this.collectionSize
-      this.paginationArray = this.paginationArrayGeneral.get(this.page)
+      this.paginationArray = this.paginationArrayGeneral.get(1)
       this.loading.next(false)
     })
   }
+  // Sort alphabetically (numbers first)
   sortAlphabetically(): any {
     return this.data.games.sort((a, b): any => {
       const x = a.Name.en.toLowerCase()
@@ -48,12 +48,11 @@ export class HomePageComponent implements OnInit {
       return 0
     })
   }
-
+  // on Click in ngb-pagination
   onPageChange(event: number): any {
-    console.log(event)
-    console.log(this.paginationArrayGeneral.get(event))
     return this.paginationArray = this.paginationArrayGeneral.get(event)
   }
+  // function realize pagination of page
   pagination(array: Array<any>, collectionSize: number): any {
     const size = this.itemsPerPage
     const arrPagination = new Map()
@@ -61,13 +60,12 @@ export class HomePageComponent implements OnInit {
     for (let i = 0; i < Math.ceil(collectionSize / size); i++){
       subarray[i] = array.slice((i * size), (i * size) + Number(size))
     }
-    console.log('subarray', subarray)
     for (let i = 0; i < subarray.length; i++) {
       arrPagination.set(i + 1, subarray[i])
     }
-    console.log('arrPagination', arrPagination)
     return arrPagination
   }
+  // on Change items per page
   onChange(event: any): any {
     this.itemsPerPage = event.target.value
     if (this.itemsPerPage <= 0) {
@@ -76,6 +74,7 @@ export class HomePageComponent implements OnInit {
     this.paginationArrayGeneral = this.pagination(this.games, this.collectionSize)
     return this.onPageChange(1)
   }
+  // on select favorite game
   onFavorite(event: any): any {
     let flag = false
     const objFavorite: any = {
@@ -84,7 +83,6 @@ export class HomePageComponent implements OnInit {
         en: ''
       }
     }
-    console.log(event)
     objFavorite.ImageFullPath = event.path.find(el => el.className === 'game__wrapper').children[0].children[0].children[0].currentSrc
     objFavorite.Name.en = event.path.find(el => el.className === 'game__actions').children[0].innerHTML
     for (let i = 0; i < localStorage.length; i++) {
@@ -93,17 +91,36 @@ export class HomePageComponent implements OnInit {
       }
     }
     if (!flag) {
-      console.log(objFavorite)
       localStorage.setItem((this.indexFavoriteItem++).toString(), JSON.stringify(objFavorite))
     }
   }
+  onSearchInput(event: any): any {
+    this.searchInput = new RegExp('\w*' + event.target.value.trim() + '\w*')
+  }
+  onSearchClick(): any {
+    const searchArray = this.games
+    const regExp = this.searchInput
+    if (regExp) {
+      if (regExp.toString() !== '/w*w*/') {
+        const dirtArray = searchArray.filter(game => regExp.test(game.Name.en) === true)
+        this.paginationArrayGeneral = this.pagination(dirtArray, dirtArray.length)
+        this.dataService.arraySize = dirtArray.length
+        this.collectionSize = dirtArray.length
+        return this.paginationArray = this.paginationArrayGeneral.get(1)
+      }
+      return 0
+    } else {
+      return 0
+    }
+  }
+  // on select filter
   onFilterChange(event: any): any {
     const filterValueDirt = event.target.value.split(' ')
     filterValueDirt.splice(0, 1)
     const filterValue = filterValueDirt.join(' ').trim()
-    console.log(filterValue)
     if (filterValue === 'All Games') {
       this.dataService.arraySize = this.games.length
+      this.paginationArrayGeneral = this.pagination(this.games, this.games.length)
       return this.paginationArray = this.paginationArrayGeneral.get(1)
     }
     if (filterValue === 'Favorites') {
@@ -122,7 +139,6 @@ export class HomePageComponent implements OnInit {
     const catInfo = this.categories.find(e => e.Name.en === filterValue)
     if (catInfo) {
       const arrayFilter = this.games.filter(game => game.CategoryID.find(e => e === catInfo.ID))
-      console.log(arrayFilter)
       this.collectionSize = arrayFilter.length
       this.paginationArrayGeneral = this.pagination(arrayFilter, this.collectionSize)
       this.dataService.arraySize = this.collectionSize
